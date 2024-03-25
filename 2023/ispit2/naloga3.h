41ec1d07c381c64b24fde53300942b14bc0eb37f
Bojan <Bojan9@users.noreply.github.com>
2024-03-25 22:17:33 +0100

    Delete 2023/ispit2 directory

--- 2023/ispit2/naloga3.h
+++
@@ -1,6 +0,0 @@
-typedef struct _Vozlisce Vozlisce;
-    struct _Vozlisce {
-    int vsebina; // »vsebina« vozlišča
-    Vozlisce* desno; // kazalec na desnega soseda ( NULL v zadnjem stolpcu)
-    Vozlisce* dol; // kazalec na spodnjega soseda ( NULL v zadnji vrstici)
-};
