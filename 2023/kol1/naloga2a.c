0a0bbeb2ac2980b3553cb38826804904c9ac2b74
Bojan <Bojan9@users.noreply.github.com>
2024-03-25 19:18:33 +0100

    Update naloga2a.c

--- 2023/kol1/naloga2a.c
+++ 2023/kol1/naloga2a.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char** poStolpcih(char** nizi, int stVhodnih, int* stIzhodnih) {
-    int max = 0;
+char** naSredino(char** nizi, int ciljnaDolzina) {
+    int len = 0;
 
-    for (int i = 0; i < stVhodnih; i++) {
-        int len = strlen(nizi[i]);
-        if (len > max) {
-            max = len;
+    for (len = 0; nizi[len] != NULL; len++) {}
+
+    char** nova = calloc(len + 1, sizeof(char*));
+
+    for (int i = 0; i < len; i++) {
+        nova[i] = calloc(ciljnaDolzina, sizeof(char));
+    }
+
+    for (int i = 0; i < len; i++) {
+        int zbor = strlen(nizi[i]);
+        int tocki = ciljnaDolzina - zbor;
+        int levo = tocki / 2;
+        int desno = tocki / 2 + tocki % 2;
+
+        for (int j = 0; j < levo; j++) {
+            nova[i][j] = '.';
+        }
+
+        for (int j = 0; j < zbor; j++) {
+            nova[i][j + levo] = nizi[i][j];
+        }
+
+        for (int j = 0; j < desno; j++) {
+            nova[i][j + levo + zbor] = '.';
         }
     }
 
-    char** nova = calloc(max, sizeof(char*));
-    for (int i = 0; i < max; i++) {
-        nova[i] = calloc(stVhodnih, sizeof(char));
-    }
-
-    for (int y = 0; y < max; y++) {
-        int p = 0;
-        for (int x = 0; x < stVhodnih; x++) {
-            if (strlen(nizi[x]) <= y) {
-                p++;
-                continue;
-            }
-            nova[y][x-p] = nizi[x][y]; 
-        }
-    }
-
-    *stIzhodnih = max;
+    nova[len] = NULL;
 
     return nova;
 }
@@ -37,18 +41,19 @@ char* NIZI[] = {
     "je",
     "kolokvij",
     "pri_P2!",
+    NULL
 };
 
 int main() {
-    int stIzhodnih = 0;
-    char** rezultat = poStolpcih(NIZI, sizeof(NIZI) / sizeof(NIZI[0]), &stIzhodnih);
-    printf("%d\n", stIzhodnih);
+    char** izhodni = naSredino(NIZI, 9);
 
-    for (int i = 0; i < stIzhodnih; i++) {
-        printf("%s\n", rezultat[i]);
-        free(rezultat[i]);
+    int stNizov = sizeof(NIZI) / sizeof(NIZI[0]) - 1;
+    for (int i = 0; i < stNizov; i++) {
+        printf("%d: \"%s\"\n", i, izhodni[i]);
+        free(izhodni[i]);
     }
-    free(rezultat);
+    printf("%s\n", (izhodni[stNizov] == NULL) ? ("NULL") : ("NAPAKA"));
 
+    free(izhodni);
     return 0;
 }
