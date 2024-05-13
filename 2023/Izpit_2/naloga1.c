184ec13610df49b2ed1824772f2834dd2eb60ada
Bojan <Bojan9@users.noreply.github.com>
2024-05-13 19:29:12 +0200

    Update naloga1.c

--- 2023/Izpit_2/naloga1.c
+++ 2023/Izpit_2/naloga1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
+int main(int argc, char** argv) {
+    FILE* vlez = fopen(argv[1], "rb");
+    FILE* izlez = fopen(argv[2], "wb");
+
+    int pred = fgetc(vlez);
+    int c;
+
+    while ((c = fgetc(vlez)) != EOF) {
+        if (!(pred == c)) {
+            fputc(pred, izlez);
+        }
+        pred = c;
+    }
+
+    fputc(pred, izlez);
+
+    fclose(vlez);
+    fclose(izlez);
+
+    return 0;
+}
